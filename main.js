f93a88eafe067d4aed911307c14144428d44ba42
cwibowo <cwibowo@beachbody.com>
2017-12-14 21:47:19 -0800

    first time making a blockchain

---
+++ main.js
@@ -0,0 +1,66 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+      this.index = index;
+      this.previousHash = previousHash;
+      this.timestamp = timestamp;
+      this.data = data;
+      this.hash = this.calculateHash();
+      this.nonce = 0;
+    }
+  
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce ++;
+            this.hash = this.calculateHash();
+        }
+        console.log('Block mined::', this.hash);
+    }
+  }
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
+    }
+    createGenesisBlock() {
+        return new Block(0, '12/14/2017', 'Genesis block', '0');
+    }
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+    isChainValid() {
+        for(let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if (currentBlock.hash != currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+let weeboCoin = new Blockchain();
+console.log('mining block 1');
+weeboCoin.addBlock(new Block(1, '12/15/2017', {amount: 4}));
+console.log('mining block 2');
+weeboCoin.addBlock(new Block(2, '12/16/2017', {amount: 7}));
+
+console.log(weeboCoin);
+
